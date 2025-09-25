7579cd8354df3b62aecb24df5e7d4c921ab38daa
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-09-25 23:04:03 +0300

    Add files via upload

---
+++ data/global.hx
@@ -0,0 +1,12 @@
+import hxvlc.util.Handle;
+import lime.graphics.Image;
+import funkin.backend.WindowUtils;
+
+function preStateSwitch() {
+	WindowUtils.winTitle = window.title = "Friday Night Funkin' HD";
+    window.setIcon(Image.fromBytes(Assets.getBytes(Paths.image('game/icon'))));
+    Main.framerateSprite.codenameBuildField.text = "Friday Night Funkin' HD";
+    FlxG.camera.bgColor = 0xFF000000;
+}
+
+function destroy() WindowUtils.winTitle = window.title = "Friday Night Funkin' - Codename Engine";
