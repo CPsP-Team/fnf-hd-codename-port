cd36797f128aabf7339667efd9ee63e584889ecf
Cream.BR <creambr6@gmail.com>
2025-09-25 22:27:52 -0300

    Before going to bed, take this

--- data/global.hx
+++ data/global.hx
@@ -1,13 +1,13 @@
 import hxvlc.util.Handle;
 import lime.graphics.Image;
 import funkin.backend.utils.WindowUtils;
+import funkin.backend.system.framerate.Framerate;
 
 function preStateSwitch() {
 	WindowUtils.winTitle = window.title = "Friday Night Funkin' HD";
     window.setIcon(Image.fromBytes(Assets.getBytes(Paths.image('game/icon'))));
-    Main.framerateSprite.codenameBuildField.text = "Friday Night Funkin' HD";
+    Framerate.codenameBuildField.text = "Friday Night Funkin' HD";
     FlxG.camera.bgColor = 0xFF000000;
 }
 
 function destroy() WindowUtils.winTitle = window.title = "Friday Night Funkin' - Codename Engine";
-
